9d097d1fd875ba84528d1f3e34e9079f6088e9d6
leorperius-alt <leo.rperius@gmail.com>
2026-08-07 23:19:35 -0300

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,4 @@
-// import { supabase } from "../supabaseClient";
+import { supabase } from "../supabaseClient";
 
 export async function getMyCompanyId() {
   const { data: auth } = await supabase.auth.getUser();
